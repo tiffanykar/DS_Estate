583984e9dff5b2231bb727c77995f46c381f6734
tiffanykar <theofania.karkantzou@gmail.com>
2024-06-10 19:09:26 +0300

    cookie addition

--- header.php
+++ header.php
@@ -3,7 +3,11 @@
         <ul>
             <li><a href="feed.php">Feed</a></li>
             <li><a href="create_listing.php">Create Listing</a></li>
+            <?php if (isset($_COOKIE['username'])): ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php else: ?>
             <li><a href="login_register.php">Login</a></li>
+        <?php endif; ?>
         </ul>
     </nav>
 </header>
