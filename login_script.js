6859c91fcfef974e11aea6a1dbf514af9f892c30
tiffanykar <theofania.karkantzou@gmail.com>
2024-06-14 16:56:20 +0300

    javascript for login forms

---
+++ login_script.js
@@ -0,0 +1,8 @@
+document.getElementById('show-register-form').addEventListener('click', function() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+});
+document.getElementById('show-login-form').addEventListener('click', function() {
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+});
